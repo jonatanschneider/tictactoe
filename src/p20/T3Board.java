a598f184f568ea94dda26c64cea83586ff2c2746
David Donges <info@david-donges.de>
2017-06-20 10:24:08 +0200

    Fix isWin
    Adjusted isWin implementation to the board base class.

--- src/p20/T3Board.java
+++ src/p20/T3Board.java
@@ -32,7 +32,7 @@ public class T3Board extends Board<Integer> {
 
 	@Override
 	public boolean isWin(){
-		if (history.size() < 5) return false;
+		if (getHistory().size() < 5) return false;
 		int[][] rows = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 		for(int[] i : rows){
 			int sum = 0;
