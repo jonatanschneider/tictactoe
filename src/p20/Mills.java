25ed1e6fe242b42a522680a246dafea80db38729
Jonatan Schneider <j.schneider1004@gmail.com>
2017-06-21 20:24:31 +0200

    changed mills style and corrected streams

--- src/p20/Mills.java
+++ src/p20/Mills.java
@@ -1,50 +1,37 @@
 package p20;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Mills extends Board<Move>{
-	public int[][] mills = {{0,1,2}, {2,3,4}, {4,6,7}, {7,8,0}, {8,9,10}, {10,11,12}, {12,13,14}, 
-			{14,15,8}, {16,17,18}, {18,19,20}, {20,21,22}, {22,23,16}, {1,9,17}, {19,11,3}, {21,13,5}, {23,15,7}};
-	public int[][] neighbours = {{1,7}, {0,2,9}, {1,3}, {2,4,11}, {3,5}, {4,6,13}, {5,7}, {0,6,15},
+	public int[][]  mills = {{1,2, 6,7}, {0,2, 9,17}, {0,1, 3,4}, {2,4, 11,19}, {2,3, 5,6},
+			{4,6, 13,21}, {4,5, 0,7}, {0,6, 15,23}, {9,10, 14,15}, {8,10, 1,17}, {8,9, 11,12},
+			{10,12, 3,19}, {10,11, 13,14}, {12,14, 5,21}, {12,13, 8,15}, {8,14, 7,23},
+			{17,18, 22,23}, {1,9, 16,18}, {16,17, 19,20}, {18,20, 3,11}, {18,19, 21,22},
+			{20,22, 5,13}, {20,21, 16,23}, {16,22, 7,15}};
+	public static int[][] neighbours = {{1,7}, {0,2,9}, {1,3}, {2,4,11}, {3,5}, {4,6,13}, {5,7}, {0,6,15},
 			{9,15}, {1,8,10,17}, {9,11}, {3,10,12,19}, {11,13}, {5,12,14,21}, {13,15}, {7,8,14,23}, {17,23}, {9,16,18},
-			{17,19}, {11,18,20}, {19,21}, {13,20,22}, {21,23}, {15,16,22}};
-
+			{17,19}, {11,18,20}, {19,21}, {13,20,22}, {21,23}, {15,16,22}};	
+	
 	@Override
 	public List<Move> moves() {
 		ArrayList<Move> moves = new ArrayList<>();
 		if(getHistory().size() < 18){
-			Arrays.stream(board)
+			IntStream.range(0, board.length)
 				.filter(i -> board[i] == 0)
 				.forEach(i -> moves.add(new Move(i)));
 		}else{
 			int turn = isBeginnersTurn() ? 1 : -1;
-			ArrayList<Move> possibleNeighbours = new ArrayList<>();
-			Arrays.stream(board)
-			.filter(i -> board[i] == turn)
-			.forEach(stone -> Arrays.stream(neighbours[stone])
-					.filter(nb -> board[nb] == 0)
-					.mapToObj(nb -> new Move(nb, stone))
-					.forEach(possibleNeighbours::add));
-			
-				
-			for(int i = 0; i < board.length; i++){
-				if(board[i] == (isBeginnersTurn() ? 1 : -1)){
-					for(int move : neighbours[i]){  //find possible neighbours
-						for(int outer = 0; outer < mills.length; outer++){
-							int sum = 0;
-							for(int inner = 0; inner < mills[outer].length; inner++){
-								sum += Math.abs(board[mills[outer][inner]]);
+			ArrayList<Move> freeNeighbours = new ArrayList<>();
+			IntStream.range(0, board.length)
+				.filter(i -> board[i] == turn)
+				.forEach(stone -> Arrays.stream(neighbours[stone])
+						.filter(nb -> board[nb] == 0)
+						.mapToObj(nb -> new Move(nb, stone))
+						.forEach(freeNeighbours::add));
 
-							}
-						}
-					}
-				}
-			}
 		}
 		return moves;
 	}
-
 }
