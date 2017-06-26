573a4aec020aa0134382d5403e1f4011284d3af4
David Donges <info@david-donges.de>
2017-06-26 09:37:32 +0200

    Fix Board hashCode implemenation
    Recent hashCode implementation wasn't sufficient for mills (overflow as there are 24 fields), now using JDK Arrays.hashCode implementation.

--- src/p20/Board.java
+++ src/p20/Board.java
@@ -1,5 +1,6 @@
 package p20;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -42,15 +43,6 @@ public abstract class Board<Move> implements ImmutableBoard<Move> {
 
     @Override
     public int hashCode() {
-        int hashCode = 0;
-        // Generate number based on the ternary numeral system to identify the play position
-        // without respecting java object references
-        for(int i = 0; i < board.length; i++) {
-            // Board's fields can hold three states -1 (player 1) / 0 (not set) / 1 (player 2)
-            // Add 1 to the field's state so the result is within 0 - 2 allowing us to
-            // build a ternary number based on the field's index
-            hashCode += Math.pow(board[i] + 1, i);
-        }
-        return hashCode;
+        return Arrays.hashCode(board);
     }
 }
