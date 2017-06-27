d2c1b5b1d81c87f6755654705942c7744173cd5d
Jonatan Schneider <j.schneider1004@gmail.com>
2017-06-27 12:30:58 +0200

    basic functionallity restored, alphaBeta still with some smaller problems

--- src/p20/AI.java
+++ src/p20/AI.java
@@ -8,38 +8,44 @@ import java.util.HashMap;
 
 public class AI<Move> {
 	private Hashtable<Integer, Integer> transTable = new Hashtable<>();
+	private Hashtable<Integer, Move> transTableMove = new Hashtable<>();
 	private int beginningDepth;
-	private Move tempBestMove;
 	private HashMap<Integer, List<Move>> killerMoves = new HashMap<>();
-	private List<Move> bestMoves = new ArrayList<>();
-	
-	public List<Move> getListOfBestMoves(){ return bestMoves; }
+	private HashMap<Integer, Move> bestMoves = new HashMap<>();
+	private HashMap<Integer, Integer> bestMovesScore = new HashMap<>();
+	public HashMap<Integer, Move> getListOfBestMoves(){ return bestMoves; }
 	
 	public Move getBestMove(ImmutableBoard<Move> b, int maxDepth) {
 		beginningDepth = b.getHistory().size();
-		for(int i = 1; i < maxDepth; i++){
+		for(int i = 1; i < maxDepth - b.getHistory().size() + 1; i++){
 			alphaBeta(b, Integer.MIN_VALUE, Integer.MAX_VALUE, i);
-			bestMoves.add(tempBestMove);
+			System.out.println(bestMoves);
 		}
-		return bestMoves.get(0);
+		return bestMoves.get(beginningDepth);
 	}
 
 	public int alphaBeta(ImmutableBoard<Move> b, int alpha, int beta, int depth) {
 		int boardDepth = b.getHistory().size();
+		if(depth == 5) System.out.println("depth = 8");
 		if (b.isWin()) return -(1000 - boardDepth);
 		if (b.isDraw()) return 0;
-		if (transTable.containsKey(b.hashCode()))return transTable.get(b.hashCode());
+		if (transTable.containsKey(b.hashCode())){
+			bestMoves.put(boardDepth, transTableMove.get(b.hashCode()));
+			bestMovesScore.put(boardDepth, transTable.get(b.hashCode()));
+			return transTable.get(b.hashCode());
+		}
 		if (depth == 0) {
-			System.out.println("MC");
-			tempBestMove = chooseBestMove(b, 100);
-			return -1;
+			Move temp = monteCarlo(b, 100);
+			bestMoves.put(boardDepth -  1, temp);
+			bestMovesScore.put(boardDepth - 1, -1);
+			return Integer.MAX_VALUE;
 		}
 		int bestValue = alpha;
 		List<Move> boardMoves = b.moves();
 		List<Move> moves = new ArrayList<>();
-		//make sure killer moves get executed first
-		if(killerMoves.get(boardDepth) != null){
-			for(Move m : killerMoves.get(boardDepth)){
+		//make sure killer moves get checked first
+		if(killerMoves.get(boardDepth + 1) != null){
+			for(Move m : killerMoves.get(boardDepth + 1)){
 				if(boardMoves.contains(m)) moves.add(0, m);
 				else moves.add(m);
 			}
@@ -50,35 +56,42 @@ public class AI<Move> {
 			if (value > bestValue) {
 				bestValue = value;
 				if (bestValue >= beta){
-					if(! killerMoves.containsKey(boardDepth)){
+					if(!killerMoves.containsKey(boardDepth)){
 						killerMoves.put(boardDepth, new ArrayList<Move>());
 					}
-					killerMoves.get(boardDepth).add(move);
+					//make sure killer moves only get added once to a list
+					if(!killerMoves.get(boardDepth).contains(move)) killerMoves.get(boardDepth).add(move);
 					break;
 				}
-				transTable.put(b.hashCode(), bestValue);
-				if (boardDepth == beginningDepth) {
-					tempBestMove = move;
+				if(bestMovesScore.containsKey(boardDepth)){
+					if(bestValue > bestMovesScore.get(boardDepth)){
+						transTable.put(b.hashCode(), bestValue);
+						transTableMove.put(b.hashCode(), move);
+						bestMoves.put(boardDepth, move);
+						bestMovesScore.put(boardDepth, bestValue);
+					}
+				}else{
+					transTable.put(b.hashCode(), bestValue);
+					transTableMove.put(b.hashCode(), move);
+					bestMoves.put(boardDepth, move);
+					bestMovesScore.put(boardDepth, bestValue);
 				}
 			}
 		}
-
 		return bestValue;
 	}
 
-	public int playRandomly(ImmutableBoard<Move> b) {
+	private int playRandomly(ImmutableBoard<Move> b) {
 		while (b.isWin() == false) {
-			if (b.isDraw())
-				return 0;
-			List<Move> moves = b.moves();
+			if (b.isDraw()) return 0;
 			Random r = new Random();
-			Move randomMove = moves.get(r.nextInt(moves.size()));
+			Move randomMove = b.moves().get(r.nextInt(b.moves().size()));
 			b = b.makeMove(randomMove);
 		}
 		return (b.isBeginnersTurn() ? -1 : 1);
 	}
 
-	public int[] simulatePlays(ImmutableBoard<Move> b, int number) {
+	private int[] simulatePlays(ImmutableBoard<Move> b, int number) {
 		int[] count = new int[3];
 		while (number > 0) {
 			count[playRandomly(b) + 1] += 1;
@@ -87,21 +100,20 @@ public class AI<Move> {
 		return count;
 	}
 
-	public int[][] evaluateMoves(ImmutableBoard<Move> b, int number) {
-		int[][] values = new int[9][];
+	private int[][] evaluateMoves(ImmutableBoard<Move> b, int number) {
+		int[][] values = new int[b.moves().size()][3];
 		for(int i = 0; i < b.moves().size(); i++){
 			values[i] = simulatePlays(b.makeMove(b.moves().get(i)), number);
 		}
 		return values;
 	}
 
-	public Move chooseBestMove(ImmutableBoard<Move> b, int number) {
-		int[] values = new int[9];
+	public Move monteCarlo(ImmutableBoard<Move> b, int number) {
+		int[] values = new int[b.moves().size()];
 		int maxValue = Integer.MIN_VALUE;
 		int indexOfMaxValue = -1;
 		int[][] evaluated = evaluateMoves(b, 1000);
-		//TODO stream?
-		for (int i = 0; i < 9; i++) {
+		for (int i = 0; i < values.length; i++) {
 			if (evaluated[i] != null) {
 				values[i] = evaluated[i][2] * (b.isBeginnersTurn() ? 1 : -1);
 				if (values[i] > maxValue) {
